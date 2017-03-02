bce9f8c0db0424a48258fbe9c38e6bdebd095548
János Tarjányi <jtarjanyi@geomant.com>
2017-03-02 21:57:58 +0100

    Readme + Manifest files

---
+++ README.rst
@@ -0,0 +1,18 @@
+ossi_tool
+-------------------------
+
+It is a Python script, to execute multiple commands on Avaya Communication Manager,
+and write output data into the CSV file for later processing. It is ideal tool to
+repeat a command many times which not available as import in Avaya Site Administration
+Tool (eg. list usage)
+
+As input arguments need to define the followings:
+    - host
+    - Username
+    - Password
+    - Input file
+    - Output file
+
+Usage example:
+
+./ossit 192.168.10.10 sampleuser -ppassword -i commands.csv -o outputfile.csv
--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='ossi_tool',
       version='0.1',
       description='Tool to creact CSV output from Avaya Communcation Manager commands. \
                     Commands are listed in an input csv',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: ISC License',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Telecommunications Industry',
+        'Topic :: Communications :: Telephony',
+        ],
       url='https://github.com/deesnow/ossi_tool',
       author='DeeSnow',
       author_email='janos.tarajnyi@gmail.com',
