33486bb5dd20605e9014e4926e3ba24cd36ec668
janos.tarjanyi@gmail.com <janos.tarjanyi@gmail.com>
2017-03-03 11:17:34 +0100

    structure fix2

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
+import re
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('ossi_tool/ossit.py').read(),
+    re.M
+    ).group(1)
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
 
 def readme():
     with open('README.rst') as f:
         return f.read()
 
 setup(name='ossi_tool',
-      version='0.1',
-      description='Tool to creact CSV output from Avaya Communcation Manager commands. \
-                    Commands are listed in an input csv',
+      version=version,
+      description='Tool to creact CSV output from Avaya Communcation Manager commands.',
+      long_description=long_descr,
       classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -16,9 +26,12 @@ setup(name='ossi_tool',
         'Topic :: Communications :: Telephony',
         ],
       url='https://github.com/deesnow/ossi_tool',
-      author='DeeSnow',
+      author='Janos Tarjanyi',
       author_email='janos.tarajnyi@gmail.com',
       license='ISC',
       packages=['ossi_tool'],
+      entry_points={
+        "console_scripts": ['ossi_tool = ossi_tool.ossit:main']
+        },
       install_requires=['pexpect'],
       zip_safe=False)
